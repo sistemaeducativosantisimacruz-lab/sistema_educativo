280d208540884f484faba3b4ab78ca4712993d18
Admin <admin@sistema.edu>
2026-06-22 09:51:29 -0500

    chore: eliminar scripts de debug y parches temporales de desarrollo

--- test_docentes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$req = request();
-$req->merge(["tab" => "docentes"]);
-$ctrl = new App\Http\Controllers\Admin\RendimientoController();
-$resp = $ctrl->index($req);
-echo "DOCENTES_COUNT=" . $resp->getData()["docentesReport"]->count();
